1234e19882b55c9f68d4889d58db9760ef141567
Elvira Gizatullina <elvirawed@gmail.com>
2023-04-05 07:03:17 -0700

    CardDeals component added with tailwind

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-function CradDeal() {
-  return <div>CradDeal</div>;
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Flexible payment <br className="sm:block hidden" /> solutions.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Make the sale in-store or online, and take credit cards, EMV chip cards,
+        gift cards, mobile wallets and more. Our payment processing is fast,
+        secure and reliable with fair and transparent rates.
+      </p>
 
-export default CradDeal;
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
