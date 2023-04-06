3f3c8a866f718bc1a197b762ef09eb3fbb5c0708
Elvira Gizatullina <elvirawed@gmail.com>
2023-04-06 07:59:20 -0700

    CTA component added

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
-function CTA() {
-  return <div>CTA</div>;
-}
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Let’s try our system now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        More than just a payment processor. Payroll, payments and POS software
+        for your budget. Accept all the ways customers want to pay: debit,
+        credit cards, Apple pay and more.
+      </p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
 
 export default CTA;
